490acb6c30396a8b3dcce7a9a27590a535b9080a
ZnaNik <57796591+ZnaNik@users.noreply.github.com>
2020-12-16 20:17:50 +0300

    forget multiple import

--- 4_5hw.py
+++ 4_5hw.py
@@ -1,3 +1,4 @@
 from functools import reduce
+from lib import multiple
 
 print(reduce(multiple,[el for el in range(100,1001,2)]))
--- 4_7hw.py
+++ 4_7hw.py
@@ -1,13 +1,16 @@
 from functools import reduce
 from lib import multiple
 
-#Вобще очень плохо понял условие, реализовал как понял
+
+# Вобще очень плохо понял условие, реализовал как понял
 def fact_range(number):
-    return [i for i in range(1, number+1)]
+    return [i for i in range(1, number + 1)]
+
 
 def mass_factorial(n):
-    for el in range(1,n+1):
+    for el in range(1, n + 1):
         yield reduce(multiple, fact_range(el))
+
 
 mass = mass_factorial(10)
 
