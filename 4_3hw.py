e741bcf69d225afe4561df3fe2aa8a48e052fd43
ZnaNik <57796591+ZnaNik@users.noreply.github.com>
2021-07-24 15:28:59 +0300

    final

--- 4_3hw.py
+++
@@ -1,2 +0,0 @@
-print([el for el in list(range(20, 240)) if el % 20 == 0 or el % 21 == 0])
-
--- 4_5hw.py
+++
@@ -1,4 +0,0 @@
-from functools import reduce
-from lib import multiple
-
-print(reduce(multiple,[el for el in range(100,1001,2)]))
